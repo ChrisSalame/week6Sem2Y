95f747ee19a97fce54852f377eccc6cd12485e89
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-02-14 10:45:30 -0500

    updatedInClass

---
+++ week6/Assets/Scripts/Tasks/sendSignalAT.cs
@@ -0,0 +1,56 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using System.Collections;
+using System.Diagnostics;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class sendSignalAT : ActionTask {
+
+		public BBParameter<bool> signal;
+		public int frameDelay;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+			signal.value = true;
+			StartCoroutine(FrameDelay());
+		}
+
+		IEnumerator FrameDelay()
+		{
+			for (int i = 0; i < frameDelay; i++) 
+			{
+				yield return null;
+			}
+			signal.value = false;
+			EndAction(true);
+
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			
+		}
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
